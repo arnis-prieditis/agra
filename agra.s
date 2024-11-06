900cf3950be1b5704ab6b8d18447a1940e2cadb8
arnis-prieditis <arnis.prieditis19@gmail.com>
2024-11-06 23:17:26 +0200

    restructure and comment pixel function

--- agra.s
+++ agra.s
@@ -15,27 +15,23 @@ setPixColor:
 
 
 pixel:
-	push	{r0-r2,lr}
+	/* ieejas parametri: x, y, color addr */
+	push	{r0-r2, lr}
 	bl	FrameBufferGetAddress
 	str	r0, [sp, #-4]!	
 	bl	FrameBufferGetWidth
 	str	r0, [sp, #-4]!	
-/* sp-> width, frame addr, x, y, color addr, lr val */
+/* sp-> width, FrameBuffer addr, x, y, color addr, lr */
+/* steka ir ierakstitas 6 vertibas */
 	ldr	r0, [sp, #8]	@ x
 	ldr	r1, [sp, #12]	@ y
-	ldr	r2, [sp]	@ width
-	mla	r3, r1, r2, r0	@ width*y + x
+	ldr	r2, [sp]		@ width
+	mla	r3, r1, r2, r0	@ width*y+x : FrameBuffer indekss, kurs jaiekraso
 	ldr	r1, [sp, #16]	@ color addr
-	ldr	r1, [r1]	@ color val
-	b	.Lcolor
-.Lend_pixel:
-	ldr	lr, [sp, #20]
-	add	sp, sp, #24
-	bx	lr
-.Lcolor:
-/* r1: color, r3: frame buffer index - sos neaiztiekam */
-	@ bic	r0, r1, #0x3FFFFFFF	@ pirmos (!!) 2 bitus saglabajam - op lauks (jo little endian??)
-	lsr	r0, r1, #30
+	ldr	r1, [r1]		@ color value
+	ldr	r2, [sp, #4]	@ FrameBuffer addr
+/* r1: color, r2: FrameBuffer addr, r3: FrameBuffer index */
+	lsr	r0, r1, #30		@ panemam tikai pirmos 2 bitus - op lauks (sakuma nevis beigas, jo little endian)
 	cmp	r0, #0
 	beq	.Lpixel_copy
 	cmp	r0, #1
@@ -44,32 +40,32 @@ pixel:
 	beq	.Lpixel_or
 	cmp	r0, #3
 	beq	.Lpixel_xor
-	b	.Lerror_pixel	@ neatbilstosa op lauka veriba
+	@ neatbilstosa op lauka veriba, ja seit tiek
 .Lerror_pixel:
 	mov	r0, #1
 	b	.Lend_pixel
 .Lpixel_copy:
-	ldr	r0, [sp, #4]		@ frame addr
-	str	r1, [r0, r3, LSL #2]	@ vnk parkope
+	str	r1, [r2, r3, LSL #2]	@ parkope/parraksta pari jauno krasu
 	b	.Lend_pixel
 .Lpixel_and:
-	ldr	r0, [sp, #4]		@ frame addr
-	ldr	r2, [r0, r3, LSL #2]	@ r2 ieliekam pasreizejo krasu
-	and	r2, r2, r1
-	str	r2, [r0, r3, LSL #2]	@ iekrasojam
+	ldr	r0, [r2, r3, LSL #2]	@ r0 ieliekam pasreizejo krasu
+	and	r0, r0, r1
+	str	r0, [r2, r3, LSL #2]	@ iekrasojam
 	b	.Lend_pixel
 .Lpixel_or:
-	ldr	r0, [sp, #4]		@ frame addr
-	ldr	r2, [r0, r3, LSL #2]	@ r2 ieliekam pasreizejo krasu
-	orr	r2, r2, r1
-	str	r2, [r0, r3, LSL #2]	@ iekrasojam
+	ldr	r0, [r2, r3, LSL #2]	@ pasreizeja krasa
+	orr	r0, r0, r1
+	str	r0, [r2, r3, LSL #2]	@ iekrasojam
 	b	.Lend_pixel
 .Lpixel_xor:
-	ldr	r0, [sp, #4]		@ frame addr
-	ldr	r2, [r0, r3, LSL #2]	@ r2 ieliekam pasreizejo krasu
-	eor	r2, r2, r1
-	str	r2, [r0, r3, LSL #2]	@ iekrasojam
+	ldr	r0, [r2, r3, LSL #2]	@ pasreizeja krasa
+	eor	r0, r0, r1
+	str	r0, [r2, r3, LSL #2]	@ iekrasojam
 	b	.Lend_pixel
+.Lend_pixel:
+	add	sp, sp, #20
+	pop	{lr}
+	bx	lr
 
 
 draw_8_symmetric_points:
